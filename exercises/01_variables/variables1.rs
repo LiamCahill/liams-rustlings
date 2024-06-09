4cbaf7ec6d9c9cdf5baa84934bf11997961eeb42
Liam <42883622+LiamCahill@users.noreply.github.com>
2024-06-09 10:17:04 -0400

    Initial commit; apply solutions to first 12 rust exercises.

--- exercises/01_variables/variables1.rs
+++ exercises/01_variables/variables1.rs
@@ -5,9 +5,13 @@
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    x = 5;
+    let x = 5;
     println!("x has the value {}", x);
+    myFuntion();
+}
+
+fn myFuntion(){
+    println!("My name is Liam.");
 }
--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,8 +3,15 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     call_me();
+}
+
+fn call_me(){
+    println!("Call on me - call me");
+    println!("Call on me - call me");
+    println!("Call on me - call me");
+    println!("Call on me - call me");
+    println!("Call on me - call me");
 }
