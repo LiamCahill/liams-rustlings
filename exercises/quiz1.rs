822486cc2eef99e76674fe07bc6efd94f5280468
Liam <42883622+LiamCahill@users.noreply.github.com>
2024-06-15 09:07:56 -0400

    Adding solutions for primitive examples 1-4.

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num:i32) -> i32 {
+    let mut cost:i32 = 2;
+    if num > 40 {
+        cost = 1
+    }
+    return num*cost;
+
+}
 
 // Don't modify this function!
 #[test]
